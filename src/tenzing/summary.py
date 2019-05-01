7f52ea21ae2ffa5e74fcbf9792817972bbfc6213
Ian Eaves <ian.k.eaves@gmail.com>
2019-05-01 16:37:01 -0500

    moved html output into code, removed yaml req

--- src/tenzing/summary.py
+++ src/tenzing/summary.py
@@ -29,70 +29,69 @@ def _get_default_config():
     return process_yaml_template(default_template)
 
 
+class renderable_config:
+    def __init__(self, data_name, template_name, extra_configs={}):
+        self.data_name = data_name
+        self.template_name = template_name
+        self.extra_configs = extra_configs
+
+    @property
+    def template(self):
+        return get_template(self.template_name)
+
+    def render(self, summary):
+        data = {}
+        data['data'] = summary.get(self.data_name)
+        data.update(self.extra_configs)
+        res = self.template.render(data=data)
+        return res
+
+
 def traverse_config(config, summary):
+    if isinstance(config, renderable_config):
+        return config.render(summary)
     if isinstance(config, list):
-        list_cmp_template = get_template('list_composition.html')
-        return list_cmp_template.render(data=[traverse_config(subconfig, summary) for subconfig in config])
+        template = get_template('list_composition.html')
+        data = [traverse_config(sub_config, summary) for sub_config in config]
+        return template.render(data=data)
     elif isinstance(config, dict):
-        if config.get('is_abstract_variable', False):
-            list_cmp_template = get_template('list_composition.html')
-            html_list = []
-            for title, val_dict in summary.get(config['data']).items():
-                new_data = {'title': summary.get('col_type_map')[title], 'data': val_dict}
-                html = get_template(config['template']).render(data=new_data)
-                new_data['title'] = title
-                new_data['data'] = html
-
-                html_list.append(new_data)
-
-            result = list_cmp_template.render(data=html_list)
-            return result
-
-        elif 'template' in config and 'data' in config:
-            template = get_template(config['template'])
-            data = config.copy()
-            data.pop('template')
-            data['data'] = summary.get(config['data'])
-            return template.render(data=data)
-        else:
-            return {key: traverse_config(value, summary) for key, value in config.items()}
+        raise Exception
+    print('shouldnt get here')
 
 
-def process_yaml_template(template):
-    if isinstance(template, list):
-        return [process_yaml_template(subtemp) for subtemp in template]
-    elif isinstance(template, dict):
-        new_template = {}
-        for key, values in template.items():
-            if 'template' in values and 'data' in values:
-                values.update({'title': key})
-                new_template[key] = values
-            else:
-                new_template[key] = process_yaml_template(values)
-        return new_template
+def render_config(config, summary):
+    base_template = get_template('base.html')
+    html = {title: traverse_config(sub_config, summary) for title, sub_config in config.items()}
+    return base_template.render(data=html)
 
 
 class summary_report:
-    def __init__(self, col_type_map, column_summary, general_summary, template=None):
-        self.column_summary = {k: self.prettify(v) for k, v in column_summary.items()}
-        self.general_summary = self.prettify(general_summary)
-        self.col_type_map = col_type_map
-        self.type_counts = Counter(self.col_type_map.values())
-        self.template = _get_default_config() if template is None else template
-        self.html = self.generate_html()
+    def __init__(self, type_map, column_summary, general_summary, template=None):
+        self.summary = {'general_summary': self.prettify(general_summary),
+                        'type_counts': Counter(type_map.values()),
+                        'column_summary': {k: self.prettify(v) for k, v in column_summary.items()},
+                        'type_map': type_map}
+
+        self.template = {'Overview': [renderable_config('general_summary', 'overview.html', {'title': 'Dataset Info'}),
+                                      renderable_config('type_counts', 'overview.html', {'title': 'Variable types'})
+                                      ],
+                         'Variable Statistics': [renderable_config('/'.join(['column_summary', title]),
+                                                                   'column_overview.html',
+                                                                   {'title': title, 'subtitle': self.summary['type_map'][title]})
+                                                 for title in self.summary['column_summary'].keys()]
+                         }
 
     @staticmethod
     def prettify(dict_):
         return {key: v if not isinstance(v, float) else round(v, 2) for key, v in dict_.items()}
 
-    def generate_html(self):
-        base_template = get_template('base.html')
-        data = traverse_config(self.template, self)
-        html_output = base_template.render(data=data)
-        return html_output
-
     def get(self, attr):
-        return getattr(self, attr)
+        for i, attr in enumerate(attr.split('/')):
+            if i == 0:
+                res = self.summary[attr]
+            else:
+                res = res[attr]
+        return res
 
     def _repr_html_(self):
-        return self.html
+        return render_config(self.template, self)
