e81c9b51c40e3be50747200c87c00c9b6df9d3e5
simon_graphkite <sfbbrugman@gmail.com>
2019-10-06 20:00:38 +0200

    Add CBR (Year/Week)

---
+++ examples/custom_relations/cbr/cbr_examens.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df = pd.read_csv('https://www.cbr.nl/web/file?uuid=22431038-3c85-4f23-8203-ce90afbecc94&owner=d214f7b5-5ce0-48dc-a521-4ef537c9d232&contentid=11702',
+                 sep=';')
+print(df.head())
