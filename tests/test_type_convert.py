df711d5b98b5164c63296b5cb30cf04dd68dc019
Simon Brugman <sbrugman@users.noreply.github.com>
2019-12-12 17:38:26 +0100

    Update test_type_convert.py
    Black

--- tests/test_type_convert.py
+++ tests/test_type_convert.py
@@ -90,7 +90,12 @@ def test_relations(source_type, relation_type, series):
 def test_consistency(series):
     typeset = visions_complete_set()
 
-    if series.name in ['timedelta_series_nat', 'date_series_nat', 'timestamp_series_nat'] and sys.version_info.major == 3 and sys.version_info.minor == 7:
+    if (
+        series.name
+        in ["timedelta_series_nat", "date_series_nat", "timestamp_series_nat"]
+        and sys.version_info.major == 3
+        and sys.version_info.minor == 7
+    ):
         pytest.skip("unsupported configuration")
 
     initial_type = typeset.detect_series_type(series.copy(deep=True))
