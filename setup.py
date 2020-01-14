d1002ebcb85e1a73f28762fe045bc7e715391a89
sbrugman <sfbbrugman@gmail.com>
2020-01-14 04:07:15 +0100

    Slim down the required dependencies

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from setuptools import setup
 from os.path import basename, splitext
 from glob import glob
-import os
 from setuptools import find_packages
 
 
@@ -13,28 +12,29 @@ install_requires = [
     "pandas>=0.25.3",
     "networkx",
     "tangled_up_in_unicode>=0.0.3",
-    "shapely",
-    "imagehash",
-    "pillow",
-    "mypy",
-    "black",
     "attr",
 ]
-
-install_requires += type_geometry_requires + type_image_path_requires
 
 extras_requires = {
     "type_geometry": type_geometry_requires,
     "type_image_path": type_image_path_requires,
     "network_plot": ["pydot", "pygraphviz", "matplotlib"],
-    "dev_docs": ["recommonmark", "sphinx_rtd_theme", "sphinx-autodoc-typehints"],
+    "dev": [
+        "black",
+        "mypy",
+        "recommonmark",
+        "sphinx_rtd_theme",
+        "sphinx-autodoc-typehints",
+    ],
 }
 
-extras_requires["all"] = (
-    extras_requires["type_geometry"] + extras_requires["type_image_path"]
-)
+extras_requires["all"] = install_requires + [
+    dependency for dependency in extras_requires.values()
+]
 
 test_requires = [
+    "mypy",
+    "black",
     "pytest>=5.2.0",
     "pytest-ordering",
     "pytest-rerunfailures",
@@ -47,7 +47,7 @@ test_requires = [
 
 setup(
     name="visions",
-    version="0.1.1",
+    version="0.1.2",
     description="Visions",
     packages=find_packages("src"),
     package_dir={"": "src"},
