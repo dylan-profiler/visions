fe0f274627244dd2da88415117b7c81d198c3379
Ian Eaves <ian.k.eaves@gmail.com>
2020-05-10 16:59:14 -0500

    timings report

--- src/visions/utils/__init__.py
+++ src/visions/utils/__init__.py
@@ -1,5 +1,6 @@
 """ Utilities suite for visions """
 from visions.utils.coercion import test_utils
+from visions.utils.profiling import profile_type
 
 # from visions.utils.images import image_utils
 from visions.utils.monkeypatches import pathlib_patch, imghdr_patch
