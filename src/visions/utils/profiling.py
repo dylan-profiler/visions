78305c2c18800b2c86df207ecc529afbe665277b
Ian Eaves <ian.k.eaves@gmail.com>
2020-05-14 19:25:53 -0500

    big O

--- src/visions/utils/profiling.py
+++ src/visions/utils/profiling.py
@@ -1,6 +1,22 @@
 import timeit
 import pandas as pd
 import numpy as np
+import big_o
+
+
+def big_o_tester(type):
+    def inner(test_series):
+        try:
+            res = big_o.big_o(
+                lambda x: x in type,
+                lambda n: test_series[0:n],
+                max_n=test_series.shape[0],
+            )
+            return res[0]
+        except np.linalg.LinAlgError:
+            return np.nan
+
+    return inner
 
 
 def profile_type(dtype, profile_data, run_count=10, normed_length=100000):
@@ -9,6 +25,7 @@ def profile_type(dtype, profile_data, run_count=10, normed_length=100000):
         for name, data in profile_data.items()
         if len(data) > 0
     }
+    big_O_test = big_o_tester(dtype)
     return [
         {
             "type": dtype,
@@ -16,6 +33,7 @@ def profile_type(dtype, profile_data, run_count=10, normed_length=100000):
             "run count": run_count,
             "average run time": timeit.timeit(lambda: data in dtype, number=run_count)
             / run_count,
+            "big O": big_O_test(data),
         }
         for name, data in profile_data.items()
     ]
--- tests/timings.py
+++ tests/timings.py
@@ -1,12 +1,30 @@
 import pandas as pd
+import numpy as np
+import big_o
 from visions.utils.profiling import profile_type
 from tests.series import get_series, get_contains_map
+
+
+def big_o_tester(type):
+    def inner(test_series):
+        try:
+            res = big_o.big_o(
+                lambda x: x in type,
+                lambda n: test_series[0:n],
+                max_n=test_series.shape[0],
+            )
+            return res[0]
+        except np.linalg.LinAlgError:
+            return np.nan
+
+    return inner
 
 
 def performance_report(membership=True):
     series_dict = {s.name: s for s in get_series()}
     conv_map = get_contains_map()
     performance_list = []
+
     for type, series_names in conv_map.items():
         if membership:
             # True: "series in type"
@@ -21,11 +39,20 @@ def performance_report(membership=True):
         performance_list.extend(profile_type(type, test_series))
 
     df = pd.DataFrame.from_records(performance_list)
-    df["type"] = df["type"].astype(str)
-    slow_df = df.groupby("type").min().reset_index()[["type", "series"]]
-    slow_df.rename(columns={"series": "slowest test"}, inplace=True)
 
+    df["type"] = df["type"].astype(str)
+    aggs = ["min", "max"]
+    agg_labels = ["worst", "best"]
+    summary_cols = ["series", "big O"]
+    agg_df = df.groupby("type").agg(aggs).reset_index()[["type"] + summary_cols]
+    agg_df.columns = ["_".join(col).strip("_") for col in agg_df.columns]
+    colrenames = {
+        f"{name}_{agg}": f"{rename} {name}"
+        for name in summary_cols
+        for agg, rename in zip(aggs, agg_labels)
+    }
+    agg_df.rename(columns=colrenames, inplace=True)
     df["normed run time"] = df["average run time"] / df["average run time"].min()
     df = df.groupby("type")["normed run time"].describe().sort_values("50%")
-    df = pd.merge(df, slow_df, on="type", how="left")
+    df = pd.merge(df, agg_df, on="type", how="left")
     return df
