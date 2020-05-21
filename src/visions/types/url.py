76a08e80189d85b4b7fb95ad99dbf85f835feebf
sbrugman <sfbbrugman@gmail.com>
2020-05-21 11:51:40 +0200

    Generic function to replace "isinstance" checking for membership with a single (or smaller amount of) "isinstance" check(s) and using significantly faster attribute checks for the rest of the series.

--- src/visions/types/url.py
+++ src/visions/types/url.py
@@ -5,8 +5,7 @@ import pandas as pd
 
 from visions.relations import IdentityRelation, InferenceRelation, TypeRelation
 from visions.types.type import VisionsBaseType
-from visions.utils.series_utils import nullable_series_contains
-from visions.utils.coercion.test_utils import isinstance_attrs
+from visions.utils.series_utils import nullable_series_contains, isinstance_attrs
 
 
 def test_url(series) -> bool:
--- src/visions/types/uuid.py
+++ src/visions/types/uuid.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 from visions.relations import IdentityRelation, InferenceRelation, TypeRelation
 from visions.types.type import VisionsBaseType
-from visions.utils.coercion.test_utils import isinstance_attrs
+from visions.utils.series_utils import isinstance_attrs
 
 
 def test_uuid(series) -> bool:
