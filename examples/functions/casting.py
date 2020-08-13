3ebcb5d86d7dc3d8613c2d17946d64104c959a6c
sbrugman <sfbbrugman@gmail.com>
2020-08-13 20:08:34 +0200

    Docs and __init__

--- examples/functions/casting.py
+++ examples/functions/casting.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-from visions.functional import cast_and_infer, cast_to_detected, detect_type, infer_type
+from visions.functional import cast_to_inferred, detect_type, infer_type
 from visions.typesets import CompleteSet
 
 # Create a DataFrame from our data
@@ -31,7 +31,7 @@ print(detect_type(df, typeset))
 # {'numbers_with_nan': Float, 'url': String, 'uuid': String}
 
 # Cast the dataframe to inferred types
-cast_df = cast_to_detected(df, typeset)
+cast_df = cast_to_inferred(df, typeset)
 print(cast_df.to_string())
 #    numbers_with_nan                                                url                                  uuid
 # 0                 3  (http, www.cwi.nl:80, /%7Eguido/Python.html, ,...  0b8a22ca-80ad-4df5-85ac-fa49c44b7ede
@@ -40,15 +40,4 @@ print(cast_df.to_string())
 
 # Print the inferred types
 print(infer_type(df, typeset))
-# {'numbers_with_nan': Integer, 'url': URL, 'uuid': UUID}
-
-# We can also choose to do this in one step
-cast_df, types = cast_and_infer(df, CompleteSet())
-print(cast_df.to_string())
-print(types)
-
-#    numbers_with_nan                                                url                                  uuid
-# 0                 3  (http, www.cwi.nl:80, /%7Eguido/Python.html, ,...  0b8a22ca-80ad-4df5-85ac-fa49c44b7ede
-# 1                 7                        (https, numpy.org, /, , , )  aaa381d6-8442-4f63-88c8-7c900e9a23c6
-# 2               NaN  (https, github.com, /pandas-profiling/pandas-p...  00000000-0000-0000-0000-000000000000
 # {'numbers_with_nan': Integer, 'url': URL, 'uuid': UUID}
--- src/visions/__init__.py
+++ src/visions/__init__.py
@@ -1,6 +1,11 @@
 """Core functionality"""
 from visions import types, typesets, utils
 from visions.dtypes.boolean import BoolDtype
-from visions.functional import cast_to_detected, detect_type, infer_type
+from visions.functional import (
+    cast_to_detected,
+    cast_to_inferred,
+    detect_type,
+    infer_type,
+)
 from visions.types import *
 from visions.typesets import *
