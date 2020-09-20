6c61ce06dfba68469fee353e0935397d1b9c2673
sbrugman <sfbbrugman@gmail.com>
2020-09-20 13:47:19 +0200

    Remove summary application

--- src/visions/application/summaries/__init__.py
+++
@@ -1,4 +0,0 @@
-from visions.application.summaries import functional, summary
-from visions.application.summaries.complete_summary import CompleteSummary
-from visions.application.summaries.frame import *
-from visions.application.summaries.series import *
--- src/visions/application/summaries/frame/__init__.py
+++
@@ -1,5 +0,0 @@
-from visions.application.summaries.frame.dataframe_summary import dataframe_summary
-
-__all__ = [
-    "dataframe_summary",
-]
