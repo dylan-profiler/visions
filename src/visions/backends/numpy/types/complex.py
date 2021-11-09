33b4c59e5742e0d6d7d402a2c16efdfdd273ce0c
Ian Eaves <ian.k.eaves@gmail.com>
2021-11-09 01:58:10 -0600

    spark requirement + complex fix (#186)
    test: fixes to bring tests up to date
    
    * spark requirement + complex fix
    * updated numpy object implementation
    * old reference to bottleneck
    * a regression in old numpy

--- src/visions/backends/numpy/types/complex.py
+++ src/visions/backends/numpy/types/complex.py
@@ -1,4 +1,5 @@
 import numpy as np
+from packaging import version
 
 from visions.backends.numpy import test_utils
 from visions.backends.numpy.array_utils import array_not_empty
@@ -6,17 +7,26 @@ from visions.backends.numpy.types.float import string_is_float
 from visions.types.complex import Complex
 from visions.types.string import String
 
+_OLD_NUMPY = version.parse(np.version.version) <= version.parse("1.19.0")
+
 
 def imaginary_in_string(array: np.ndarray, imaginary_indicator: tuple = ("j", "i")):
     return any(any(v in s for v in imaginary_indicator) for s in array)
 
 
+@Complex.register_transformer(String, np.ndarray)
+def string_to_complex(array: np.array, state: dict) -> np.ndarray:
+    if _OLD_NUMPY:
+        return np.array([complex(v) for v in array])
+    else:
+        return array.astype(complex)
+
+
 @Complex.register_relationship(String, np.ndarray)
 def string_is_complex(array: np.ndarray, state: dict) -> bool:
-    def f(arr: np.array) -> np.array:
-        return arr.astype(np.complexfloating)
-
-    coerced_array = test_utils.option_coercion_evaluator(f)(array)
+    coerced_array = test_utils.option_coercion_evaluator(
+        lambda x: string_to_complex(x, state)
+    )(array)
     return (
         coerced_array is not None
         and not string_is_float(array, state)
@@ -24,12 +34,7 @@ def string_is_complex(array: np.ndarray, state: dict) -> bool:
     )
 
 
-@Complex.register_transformer(String, np.ndarray)
-def string_to_complex(array: np.array, state: dict) -> np.ndarray:
-    return array.astype(np.complexfloating)
-
-
 @Complex.contains_op.register
 @array_not_empty
 def complex_contains(array: np.ndarray, state: dict) -> bool:
-    return np.issubdtype(array.dtype, np.complexfloating)
+    return np.issubdtype(array.dtype, complex)
